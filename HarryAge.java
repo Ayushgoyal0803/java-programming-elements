3977339a9f27590d31100ef3f109a32e3adff985
Ayush <ayushgoyal0808@gmail.com>
2025-02-28 11:11:53 +0530

    [Ayush Goyal] Add: Printing harry's age

---
+++ HarryAge.java
@@ -0,0 +1,9 @@
+class HarryAge {
+	
+	public static void main(String[] args){
+		int age = 2024 - 2000; // Given harry birth is in 2000
+		System.out.println("Harry's age in 2024 : " + age + " years");
+		
+	}
+
+}
